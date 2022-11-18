23468fa5a700e064a1cd57926530fb2478892897
Esther Rose <estherrose046@gmail.com>
2022-11-17 20:00:24 -0600

    class to functional notation in CreateSubject

--- screens/CreateSubject.js
+++ screens/CreateSubject.js
@@ -3,64 +3,55 @@ import { Component } from "react";
 
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
+const CreateSubject = () => {
+  const [subjectName, setSubjectName] = useState("");
+  const handleSubjectName = (e) => setSubjectName(e.target.value);
 
+  const [template, setTemplate] = useState(null);
+  const handleSubjectTemplate = (e) => setTemplate(e.target.value);
 
+  //would useState(behaviors) make it display what we want?
+  const [behaviors, setBehaviors] = useState("");
+  const handleBehaviors = (e) => setTemplate(e.target.value);
 
+  const handleSubmitSubject = (event) => {
+    event.preventDefault();
+    const requestBody = { subjectName, template };
 
-
-class CreateSubject extends Component {
-  render() {
-    const [subjectName, setSubjectName] = useState("");
-    const handleSubjectName = (e) => setSubjectName(e.target.value);
-    
-    const [template, setTemplate] = useState(null);
-    const handleSubjectTemplate = (e) => setTemplate(e.target.value);
-    
-    //would useState(behaviors) make it display what we want?
-    const [behaviors, setBehaviors] = useState("");
-    const handleBehaviors = (e) => setTemplate(e.target.value);
-
-    const handleSubmitSubject = event => {
-      event.preventDefault();
-      const requestBody={ subjectName, template }
-    
-      axios.post(`${API_URL}/api/-----------`, requestBody, {
-          // headers: { Authorization: `Bearer ${localStorage.getItem('authToken')}`}
-    })
-    .then((response) =>{
-    console.log(response.data)
-    //nav to new active session
-    })
-    .catch((err) => setMessage(err.response.data.message))
-    }
-    return (
-      <View style={styles.container}>
-        
-        <TextInput
-                style={styles.inputText}
-                placeholder="Enter Subject Name"
-                value={subjectName}
-                onChangeText={subjectName => setSubjectName(subjectName)}
-/>
-
-{/* do we want input here? Maybe yes but with drop down menu of selections */}
-        <TextInput
+    axios
+      .post(`${API_URL}/api/-----------`, requestBody, {
+        // headers: { Authorization: `Bearer ${localStorage.getItem('authToken')}`}
+      })
+      .then((response) => {
+        console.log(response.data);
+        //nav to new active session
+      })
+      .catch((err) => setMessage(err.response.data.message));
+  };
+  return (
+    <View style={styles.container}>
+      <TextInput
         style={styles.inputText}
-        placeholder="Add Behaviors"
-        value={behaviors}/>
-        <Button title="Add Behaviors" 
-          //do we need to handleBehaviors here or submit them?
-          onPress={handleBehaviors}
-        />
-        <Button
-          title=""
-          // onPress={handleSubmitSubject}
-          onPress={() => this.props.navigation.navigate("ActiveSession")}
-        />
-      </View>
-    );
-  }
-}
+        placeholder="Enter Subject Name"
+        value={subjectName}
+        onChangeText={(subjectName) => setSubjectName(subjectName)}
+      />
+      <TextInput
+        style={styles.inputText}
+        placeholder="Notes"
+        value={subjectName}
+        onChangeText={(subjectName) => setSubjectName(subjectName)}
+      />
+      {/* do we want input here? Maybe yes but with drop down menu of selections */}
+
+      <Button
+        title=""
+        // onPress={handleSubmitSubject}
+        onPress={() => this.props.navigation.navigate("ActiveSession")}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -70,8 +61,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 
-  inputText:{
-    height:45,
+  inputText: {
+    height: 45,
     marginBottom: 15,
   },
 });
